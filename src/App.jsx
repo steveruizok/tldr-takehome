10712a955948835d71530ea289236fb3c821ce0c
Taha <98838967+Taha-Hassan-Git@users.noreply.github.com>
2023-03-28 21:55:42 +0100

    begin adding effect and ref hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
+import { useEffect, useRef } from 'react'
 import './App.css'
 import { emojis } from './assets/emojis'
 
 function App() {
 
+  const emojiRef = useRef(null)
+  const canvasRef = useRef(null)
+
+  useEffect(() => {
+    if (!emojiRef.current || !canvasRef.current) return; 
+
+    const emoji = emojiRef.current;
+    const canvas = canvasRef.current;
+
+    const onMouseDown = () => {
+      console.log('clicked');
+    }
+
+    emoji.addEventListener('mousedown', onMouseDown)
+
+    const cleanup = () => {
+      emoji.removeEventListener('mousedown', onMouseDown);
+    }
+  }, [])
+
   return (
     <>
-    <main className="Canvas"></main>
+    <main ref={canvasRef} className="Canvas"></main>
     <footer className="Footer">
-    {emojis.map(emoji => (
-    <span className="emoji" title={`&#${emoji};`}>{String.fromCodePoint(emoji)}</span>))}
+    <span ref={emojiRef} className="emoji">&#128513;</span>
+    {/* {emojis.map(emoji => (
+    <span ref="emojiRef" className="emoji" title={`&#${emoji};`}>{String.fromCodePoint(emoji)}</span>))} */}
     </footer>
     </>
   )
--- src/assets/emojis.js
+++ src/assets/emojis.js
@@ -2,7 +2,7 @@ const emojiRange = [
     [128513, 128591],
     [128640, 128704],
   ]
-  
+
 export const emojis = emojiRange
     .map(([start, end]) =>
       Array.from({ length: end - start }, (x, i) => i + start)
