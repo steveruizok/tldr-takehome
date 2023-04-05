3e240832b277efd86b7eb59c44939296f0d96992
Taha <98838967+Taha-Hassan-Git@users.noreply.github.com>
2023-04-05 22:14:23 +0100

    css cleanup

--- src/Components/Canvas.jsx
+++ src/Components/Canvas.jsx
@@ -9,40 +9,37 @@ export function Canvas({
   offset,
   randomRotation,
 }) {
-    
-    
-    const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
-    const [stickers, setStickers] = useState([]);
+  const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
+  const [stickers, setStickers] = useState([]);
 
-    const handleMouseMove = (event) => {
-        const { clientX, clientY } = event;
-        setCursorPosition({ x: clientX - offset, y: clientY - offset });
-      };
+  const handleMouseMove = (event) => {
+    const { clientX, clientY } = event;
+    setCursorPosition({ x: clientX - offset, y: clientY - offset });
+  };
 
-      
-    const handleCanvasClick = (event) => {
-        if (selectedEmoji !== trash) {
-          const { clientX, clientY } = event;
-          const newSticker = {
-            emoji: selectedEmoji,
-            x: clientX,
-            y: clientY,
-            rotation: rotation,
-          };
-          setStickers([...stickers, newSticker]);
-        }
-        setRotation(randomRotation());
+  const handleCanvasClick = (event) => {
+    if (selectedEmoji !== trash) {
+      const { clientX, clientY } = event;
+      const newSticker = {
+        emoji: selectedEmoji,
+        x: clientX,
+        y: clientY,
+        rotation: rotation,
       };
+      setStickers([...stickers, newSticker]);
+    }
+    setRotation(randomRotation());
+  };
 
-      const handleDelete = (index) => {
-        if (selectedEmoji === trash) {
-          setStickers(stickers.filter((_, stickerIndex) => stickerIndex !== index));
-        }
-      };
+  const handleDelete = (index) => {
+    if (selectedEmoji === trash) {
+      setStickers(stickers.filter((_, stickerIndex) => stickerIndex !== index));
+    }
+  };
 
   return (
     <div
-      className={`canvas ${selectedEmoji === trash ? " deleteMode" : ""}`}
+      className={`canvas ${selectedEmoji === trash ? " delete-mode" : ""}`}
       onClick={handleCanvasClick}
       onMouseMove={handleMouseMove}
     >
